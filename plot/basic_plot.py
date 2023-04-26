5140449e5e17f356e94ba7b4c8f1e088b92fb980
Chrisbmc <chrisbmc@gmail.com>
2023-04-26 12:36:53 +0100

    add basic_plot.py

---
+++ plot/basic_plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Data for x-axis and y-axis
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Plot the data on the axis
+plt.plot(x, y)
+
+# Set labels for x-axis and y-axis
+plt.set_xlabel('X-axis Label')
+plt.set_ylabel('Y-axis Label')
+
+# Set title for the plot
+plt.set_title('Line Graph Example')
+
+# Display the plot
+plt.show()
